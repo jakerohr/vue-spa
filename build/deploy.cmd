59d6139ca553118c1fd861b4130253fbe50ef0fd
Jake Rohr <jrohr@c02r20peg8wl.reicorpnet.com>
2018-06-26 23:05:49 -0700

    fix second typo in deploy command

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -8,7 +8,7 @@ xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
 xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html*
 xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js*
 xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*
-xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\build\web.config*
+xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\web.config*
 
 call npm install --only=production
 echo Deployed.
